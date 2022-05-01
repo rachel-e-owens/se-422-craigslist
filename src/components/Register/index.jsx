9109afce7a3fc819e76b76dcafc3940ded5f8e0b
Rachel Owens <rachel.e.owens@gmail.com>
2022-04-30 20:00:01 -0500

    trigger deploy

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -8,7 +8,7 @@ const SignIn = ({ onSignIn }) => {
     const navigate = useNavigate();
 
     const register = async () => {
-        console.log("need to setup registration");
+        console.log("need to setup registration!");
 
     };
 
